f3d1ad2e64897b38e8d788a5b5523b2742db3446
yashodgayashan <yashodgayashan@gmail.com>
2020-03-29 12:01:25 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Corona App",
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("Corona Information"),
+    ),
+  ),
+));
+
